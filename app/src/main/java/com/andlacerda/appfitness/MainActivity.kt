cb236782bc9e2aed5a5da1c17a73637dbbc55ca3
Anderson <andersonlacerdadev@gmail.com>
2025-09-07 18:07:38 -0300

    Added the second screen and the basic form with validation

--- app/src/main/java/com/andlacerda/appfitness/MainActivity.kt
+++ app/src/main/java/com/andlacerda/appfitness/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.andlacerda.appfitness
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnImc: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnImc = findViewById(R.id.btn_imc)
+
+        btnImc.setOnClickListener {
+            val i = Intent(this, imcActivity::class.java)
+            startActivity(i)
         }
     }
 }
---
+++ app/src/main/java/com/andlacerda/appfitness/imcActivity.kt
@@ -0,0 +1,45 @@
+package com.andlacerda.appfitness
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class imcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_imc)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+        btnSend.setOnClickListener {
+            if (!validate()) {
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+        }
+    }
+
+    private fun validate() : Boolean {
+        return(editWeight.text.toString().isNotEmpty()
+            && editHeight.text.toString().isNotEmpty()
+            && !editWeight.text.toString().startsWith("0")
+            && !editHeight.text.toString().startsWith("0"))
+            return true
+    }
+}
