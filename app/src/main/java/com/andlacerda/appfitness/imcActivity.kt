32a0570e6e2df9d44ba94e8ad6c7ee171a2fa40c
Anderson <andersonlacerdadev@gmail.com>
2025-09-08 19:05:53 -0300

    Added a popup windows that shows the Imc results, with a OK button

--- app/src/main/java/com/andlacerda/appfitness/imcActivity.kt
+++ app/src/main/java/com/andlacerda/appfitness/imcActivity.kt
@@ -1,10 +1,13 @@
 package com.andlacerda.appfitness
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -32,9 +35,41 @@ class imcActivity : AppCompatActivity() {
                 Toast.makeText(this, R.string.fields_message, Toast.LENGTH_LONG).show()
                 return@setOnClickListener
             }
+
+            val weight = editWeight.text.toString().toInt()
+            val height = editHeight.text.toString().toInt()
+
+            val result = calculateImc(weight, height)
+            val imcResponseId = imcResponse(result)
+
+            val dialog = AlertDialog.Builder(this)
+
+            dialog.setTitle(getString(R.string.imc_response, result))
+            dialog.setMessage(imcResponseId)
+            dialog.setPositiveButton(android.R.string.ok){ dialog, which ->
+
+            }
+            val d = dialog.create()
+            d.show()
         }
     }
 
+    @StringRes
+    private fun imcResponse(imc: Double) : Int {
+        return when { imc < 15.0 -> R.string.imc_severely_low_weight
+                        imc < 16.0 -> R.string.imc_very_low_weight
+                        imc < 18.5 -> R.string.imc_low_weight
+                        imc < 25.0 -> R.string.normal
+                        imc < 30.0 -> R.string.imc_high_weight
+                        imc < 35.0 -> R.string.imc_so_high_weight
+                        imc < 40.0 -> R.string.imc_severely_high_weight
+                        else -> R.string.imc_extreme_weight
+        }
+    }
+
+    private fun calculateImc(weight: Int, height: Int): Double {
+        return weight / ( (height / 100.0) * (height / 100.0))
+    }
     private fun validate() : Boolean {
         return(editWeight.text.toString().isNotEmpty()
             && editHeight.text.toString().isNotEmpty()
